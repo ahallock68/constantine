908f1644e63e0dc228e55aeb11a026d07a297411
ahallock68 <ahallock68@yahoo.com>
2023-06-09 11:21:42 -0700

    Implement React-Router  and update pages

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,5 +1,6 @@
 import { Navbar, Nav, Container } from "react-bootstrap";
 import { FaShoppingCart, FaUser } from "react-icons/fa";
+import { LinkContainer } from "react-router-bootstrap";
 import logo from "../assets/logo.jpg";
 
 const Header = () => {
@@ -7,20 +8,26 @@ const Header = () => {
     <header>
       <Navbar bg="dark" variant="dark" expand="md" collapseOnSelect>
         <Container>
-          <Navbar.Brand href="/">
-            {/* Replace thelogo with a better logo */}
-            <img src={logo} alt="Constantine" height={37} width={37} />
-            <span> Constantine</span>
-          </Navbar.Brand>
+          <LinkContainer to="/">
+            <Navbar.Brand>
+              {/* Replace thelogo with a better logo */}
+              <img src={logo} alt="Constantine" height={37} width={37} />
+              <span> Constantine</span>
+            </Navbar.Brand>
+          </LinkContainer>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto">
-              <Nav.Link href="/cart">
-                <FaShoppingCart /> Cart
-              </Nav.Link>
-              <Nav.Link href="/login">
-                <FaUser /> Sign In
-              </Nav.Link>
+              <LinkContainer to="/cart">
+                <Nav.Link>
+                  <FaShoppingCart /> Cart
+                </Nav.Link>
+              </LinkContainer>
+              <LinkContainer to="/login">
+                <Nav.Link>
+                  <FaUser /> Sign In
+                </Nav.Link>
+              </LinkContainer>
             </Nav>
           </Navbar.Collapse>
         </Container>
