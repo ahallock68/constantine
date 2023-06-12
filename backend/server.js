8d76c0c6a5ae37c53c1b516ec4b17779c57cb14e
Andrew Hallock <ahallock68@yahoo.com>
2023-06-11 20:51:07 -0700

    Setup MongoDB and Connection

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,12 @@
 import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+import connectDB from "./config/db.js";
 import products from "./data/products.js";
 
-const port = 5000;
+const port = process.env.PORT || 5001;
+
+connectDB();
 
 const app = express();
 
